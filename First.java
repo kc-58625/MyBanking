6a6b247c339916fe7ccc76a6eb773b04d4404a21
kc58625 <2017.kunal.chugriya@ves.ac.in>
2021-07-29 10:35:17 +0530

    This is our first commit

---
+++ First.java
@@ -0,0 +1,11 @@
+class First{
+
+public static void main(String args[])
+{
+
+	System.out.println("First git.");
+	System.out.println("Hello gits...");
+
+}
+
+}
---
+++ Second.java
@@ -0,0 +1,9 @@
+class Second
+{
+
+	public static void main(String args[])
+
+		{
+			System.out.println("Second code...");
+		}
+}
